474162abbd8bf10c18274596d8af0e7cee0d6049
김경호 <rlarudgh2017@gmail.com>
2021-04-11 13:10:49 +0000

    UPDATE index, style, timeLeft
    fixed bug: addEventListener does not get submitting because of html code
    formatting

--- js/timeLeft.js
+++ js/timeLeft.js
@@ -1,33 +1,46 @@
 const yearToSecond = 365 * 24 * 60 * 60;
-const MAX_LIFE_TIME = 82 * yearToSecond; //according to 2020 Korean life time expendancy 
-const USER_AGE = "user_age";
-const ageQuestion = document.querySelector("left-age-question");
-const leftSecs = document.querySelector("left-seconds");
+const MAX_LIFE_TIME = 82 * yearToSecond; //according to 2020 Korean life time expendancy
+const USER_AGE = 'user_age';
+const form = document.querySelector(".questions");
+const ageQuestion = document.querySelector('.left-age-question');
+const leftSecs = document.querySelector('.left-seconds');
 const localStorage = window.localStorage;
 
 function saveAge(age) {
-    localStorage.setItem(USER_AGE, age);
-}
-
-function changeDisplay(age) {
-    ageQuestion.classList.toggle("sr-only");
-    leftSecs.innerHTML = `${MAX_LIFE_TIME - age * yearToSecond} SECONDS`;
-    leftSecs.classList.toggle("sr-only");
+	localStorage.setItem(USER_AGE, age);
 }
 
 function handleAge(event) {
-    let age = ageQuestion.value;
-    event.preventDefault();
-    saveAge(age);
-    changeDisplay(age);
+	event.preventDefault();
+	const age = ageQuestion.value;
+	console.log("submit event entered");
+	showLeft(age);
+	saveAge(age);
+}
+
+function showLeft(age) {
+	ageQuestion.classList.add("sr-only");
+	leftSecs.classList.remove("sr-only");
+	leftSecs.innerText = `${MAX_LIFE_TIME - age * yearToSecond} Sec.`;
+}
+
+function askForAge() {
+	ageQuestion.classList.remove("sr-only");
+	form.addEventListener("submit", handleAge);
+}
+
+function loadTime() {
+	const age = localStorage.getItem(USER_AGE);
+	if (age === null) {
+		askForAge();
+	}
+	else {
+		showLeft(age);
+	}
 }
 
 function init() {
-    let age = localStorage.getItem(USER_AGE);
-    if (age === null)
-        document.querySelector(".question").addEventListener("submit", handleAge);
-    else
-        changeDisplay(age);
+	loadTime();
 }
 
 init();
