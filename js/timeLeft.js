423aed5cc45807711b6297213e1b07855fe802aa
김경호 <rlarudgh2017@gmail.com>
2021-04-12 13:07:23 +0000

    UPDATE index, timeLeft, style
    bug fixed by seperating input with two forms
    when there are more than two inputs in one form, enter key do not submit
    to their form

--- js/timeLeft.js
+++ js/timeLeft.js
@@ -1,10 +1,17 @@
 const yearToSecond = 365 * 24 * 60 * 60;
 const MAX_LIFE_TIME = 82 * yearToSecond; //according to 2020 Korean life time expendancy
 const USER_AGE = 'user_age';
-const form = document.querySelector(".questions");
+const formAge = document.querySelector('.age-questions');
 const ageQuestion = document.querySelector('.left-age-question');
 const leftSecs = document.querySelector('.left-seconds');
 const localStorage = window.localStorage;
+
+// ageQuestion.addEventListener('keyup', function (event) {
+// 	if (event.code === 'Enter') {
+// 		event.preventDefault();
+// 		form.submit();
+// 	}
+// });
 
 function saveAge(age) {
 	localStorage.setItem(USER_AGE, age);
@@ -13,28 +20,27 @@ function saveAge(age) {
 function handleAge(event) {
 	event.preventDefault();
 	const age = ageQuestion.value;
-	console.log("submit event entered");
+	console.log('submit event entered');
 	showLeft(age);
 	saveAge(age);
 }
 
 function showLeft(age) {
-	ageQuestion.classList.add("sr-only");
-	leftSecs.classList.remove("sr-only");
+	ageQuestion.classList.add('sr-only');
+	leftSecs.classList.remove('sr-only');
 	leftSecs.innerText = `${MAX_LIFE_TIME - age * yearToSecond} Sec.`;
 }
 
 function askForAge() {
-	ageQuestion.classList.remove("sr-only");
-	form.addEventListener("submit", handleAge);
+	ageQuestion.classList.remove('sr-only');
+	formAge.addEventListener('submit', handleAge);
 }
 
 function loadTime() {
 	const age = localStorage.getItem(USER_AGE);
 	if (age === null) {
 		askForAge();
-	}
-	else {
+	} else {
 		showLeft(age);
 	}
 }
