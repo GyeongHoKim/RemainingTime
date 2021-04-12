4f1cd393886e3ba0a38de15155d77264c4a9c5c7
김경호 <rlarudgh2017@gmail.com>
2021-04-12 13:23:02 +0000

    BACKGROUND IMG CHANGED

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -1,4 +1,4 @@
-var bgColorArray = ['assets/images/redMoon.jpg','assets/images/scentCandle.jpg','assets/images/star.jpg'],
+var bgColorArray = ['assets/images/blackTexture.jpg','assets/images/scentCandle.jpg','assets/images/star.jpg'],
     selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 
 document.body.style.backgroundImage = 'url(' + selectBG + ')';
